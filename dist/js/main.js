4e53cbd191fe7861060fd62b07321b5639e5bb4e
aykhsn <ayakah005@gmail.com>
2022-04-13 12:41:36 +0900

    add switchNavigationButton function

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,11 @@
+const navigation = {
+	current: 'Top',
+	items: ['About', 'Skills', 'Works', 'Other', 'Contact']
+};
 let element = {};
-let position = {};
+let position = {
+	items: {}
+};
 
 /**********************
  Event
@@ -7,6 +13,7 @@ let position = {};
 window.addEventListener('DOMContentLoaded', () => {
 	element.loading = document.getElementById('loading');
 	element.body = document.querySelector('body');
+	element.navigationButton = document.querySelectorAll('#Navigation button');
 	element.barGraph = document.getElementById('barGraph');
 	element.navigation = document.getElementById('Navigation');
 	element.about = document.getElementById('About');
@@ -15,20 +22,34 @@ window.addEventListener('DOMContentLoaded', () => {
 window.onload = () => {
 	// 読み込み完了後にローディング画面を閉じる
 	closeLoadingScreen();
-}
 
-window.addEventListener('scroll', () => {
-	position.scrollTop = window.scrollY;
-	position.windowHeight = window.innerHeight;
+	// 指定要素の位置を判定する
+	setElementPosition();
 
-	// グラフが画面に入ったらアニメーションを開始する
-	const slideAnimationTriggerRect = element.barGraph.getBoundingClientRect();
-	startSlideAnimation(slideAnimationTriggerRect);
+	window.addEventListener('scroll', () => {
+		position.scrollTop = window.scrollY;
+		position.windowHeight = window.innerHeight;
 
-	// Aboutが画面に入る位置でナビゲーション表示を切り替える
-	const mobileNavigationTriggerRect = element.about.getBoundingClientRect();
-	switchMobileNavigation(mobileNavigationTriggerRect);
-});
+		// グラフが画面に入ったらアニメーションを開始する
+		const slideAnimationTriggerRect = element.barGraph.getBoundingClientRect();
+		startSlideAnimation(slideAnimationTriggerRect);
+
+		// Aboutが画面に入る位置でナビゲーション表示を切り替える
+		const mobileNavigationTriggerRect = element.about.getBoundingClientRect();
+		switchMobileNavigation(mobileNavigationTriggerRect);
+
+		// ナビゲーションの現在位置を切り替える
+		switchNavigationButton(navigation.current);
+	});
+};
+
+window.addEventListener('resize', () => {
+	// 指定要素の位置を判定する
+	setElementPosition();
+
+	// ナビゲーションの現在位置を切り替える
+	switchNavigationButton(navigation.current);
+});	
 
 /**********************
  Loading Screen
@@ -47,11 +68,44 @@ const startSlideAnimation = (triggerRect) => {
 };
 
 /**********************
- Mobile Navigation
+ Navigation
 ***********************/
 const switchMobileNavigation = (triggerRect) => {
 	position.scrollTop > triggerRect.top ? element.navigation.classList.add('show') : element.navigation.classList.remove('show');
 };
+
+const setElementPosition = () => {
+	for(let item of navigation.items) {
+		const targetElement = document.getElementById(item);
+		const targetElementRect = targetElement.getBoundingClientRect();
+		const offset = 340;
+		const targePositionTop = targetElementRect.top + window.pageYOffset - window.innerHeight + offset;
+
+		position.items[item] = targePositionTop;
+	}
+};
+
+const setCurrentPosition = () => {
+	for (let key of Object.keys(position.items)) {
+		if(position.scrollTop > position.items[key]) {
+			navigation.current = key;
+		}
+	}
+
+	if (position.scrollTop < 10) {
+		navigation.current = 'Top';
+	}
+};
+
+const switchNavigationButton = (currentId) => {
+	setCurrentPosition();
+
+	element.navigationButton.forEach(function (element) {
+	    element.classList.remove('active');
+	});
+
+	document.getElementsByName(currentId)[0].classList.add('active');
+}
 
 /**********************
  Vue.js
@@ -61,7 +115,7 @@ const vm = new Vue ({
 
 	data: {
 		isEn : true,
-		navigationItems : ['About', 'Skills', 'Works', 'Other', 'Contact']
+		navigationItems : navigation.items
 	},
 
 	methods: {
@@ -69,10 +123,13 @@ const vm = new Vue ({
 			this.isEn = isEn;
 		},
 
+		/**********************
+		 Scroll Event
+		***********************/
 		scrollToTarget: function(id) {
 			const targetElement = document.getElementById(id);
 			const targetElementRect = targetElement.getBoundingClientRect();
-			const offset = window.innerWidth > 768 ? 0 : 36; 
+			const offset = window.innerWidth > 768 ? 0 : 36;
 			const targePositionTop = targetElementRect.top + window.pageYOffset - offset;
 
 			window.scrollTo({
